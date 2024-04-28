c8135125515d9d96b77000173c45a1dbf3af5884
yaredybk <yb12ybk@gmail.com>
2024-04-28 12:11:23 -0700

    file/s updated

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,19 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import String
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -23,11 +32,11 @@ class BaseModel:
                 self.id = str(uuid.uuid4())
             if 'created_at' in kwargs.keys():
                 print(type(kwargs))
-                self['created_at'] = datetime.fromisoformat(kwargs["created_at"])
+                self.created_at = datetime.fromisoformat(kwargs["created_at"])
             else:
                 self.created_at = datetime.now()
             if 'updated_at' in kwargs.keys():
-                self['updated_at'] = datetime.fromisoformat(kwargs["updated_at"])
+                self.updated_at = datetime.fromisoformat(kwargs["updated_at"])
             else:
                 self.updated_at = datetime.now()
 
