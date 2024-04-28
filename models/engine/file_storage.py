32c6d5684d19bed9f439a1518019699e2d717e0e
yaredybk <yb12ybk@gmail.com>
2024-04-28 14:13:04 -0700

    hot fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -53,6 +53,7 @@ class FileStorage:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
+
     def close(self):
         """Call the reload method."""
         self.reload()
