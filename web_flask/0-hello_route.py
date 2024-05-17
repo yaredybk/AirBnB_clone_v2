da96569162a0a7d28b6ee256d916ef906af5476b
yaredybk <yb12ybk@gmail.com>
2024-05-16 21:45:49 -0700

    file/s updated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,10 +2,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def home():
     # home page route
     return ("Hello HBNB!")
+
 
 if __name__ == __main__:
     # start server
