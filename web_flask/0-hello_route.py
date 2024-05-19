e19f162b90c231e27d0e416458c885b4a1d61d54
yaredybk <yb12ybk@gmail.com>
2024-05-19 09:42:11 -0700

    hot fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -15,3 +15,4 @@ def home():
 if __name__ == "__main__":
     """ start server"""
     app.run(host='0.0.0.0', port=5000)
+
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def hbnb():
     """ hbnb page route """
-    return "C {}".format(escape(text.replace('_', ' '))
+    return "C {}".format(escape(text.replace('_', ' ')))
 
 
 if __name__ == "__main__":
