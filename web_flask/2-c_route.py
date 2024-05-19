73de5383a22748aebf1fc28298db5303e99a04e4
yaredybk <yb12ybk@gmail.com>
2024-05-19 00:29:42 -0700

    hot fix

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -8,22 +8,22 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
-    # home page route
+    """ home page route """
     return ("Hello HBNB!")
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    # hbnb page route
+    """ hbnb page route """
     return ("HBNB")
 
 
 @app.route("/c/<tmp>", strict_slashes=False)
 def hbnb():
-    # hbnb page route
+    """ hbnb page route """
     return (f"C {escape(tmp).replace('_', ' ')}")
 
 
 if __name__ == '__main__':
-    # start server
+    """ start server """
     app.run(host='0.0.0.0', port=5000)
--- web_flask/b/6-number_odd_or_even.py
+++ web_flask/b/6-number_odd_or_even.py
@@ -8,40 +8,40 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
-    # home page route
+    """home page route"""
     return ("Hello HBNB!")
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    # hbnb page route
+    """hbnb page route"""
     return ("HBNB")
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def hbnb():
-    # hbnb page route
+    """hbnb page route"""
     return (f"C {escape(text).replace('_', ' ')}")
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def hbnb():
-    # hbnb page route
+    """hbnb page route"""
     return (f"{n} is a number")
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def hbnb():
-    # hbnb page route
+    """hbnb page route"""
     return render_template('5-number.html', number=n)
 
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def hbnb():
-    # hbnb page route
+    """hbnb page route"""
     return render_template('6-number_odd_or_even.html', number=n)
 
 
 if __name__ == '__main__':
-    # start server
+    """start server"""
     app.run(host='0.0.0.0', port=5000)
