31d24510570fdbcd5842a63992f5a06451000ec8
yaredybk <yb12ybk@gmail.com>
2024-05-19 09:02:03 -0700

    hot fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -16,6 +16,6 @@ def hbnb():
     return ("HBNB")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """ start server """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
